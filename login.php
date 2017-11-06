4dc1f9c96c6f20fda1d4b7d6093117fb4bcc4818
OSSYULYYZ <yimeng.zhu@mail.mcgill.ca>
2017-11-06 18:53:29 -0500

    SQL working with XAMPP

--- login.php
+++ login.php
@@ -9,17 +9,50 @@
 </form>
 
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App(array(
+    'debug' => true
+));
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $name = htmlspecialchars($_REQUEST['fname']); 
-    $password = htmlspecialchars($_REQUEST['fpassword']);
-    if (empty($name) || empty($password)) {
+    $pass = htmlspecialchars($_REQUEST['fpassword']);
+    
+    if (empty($name) || empty($pass))
+    {
     	echo "ERROR";
-    } else {
-        echo $name;
-        echo $password;
-    }
-}
+    } 
+    else 
+    {
+      // Add to the SQL Database
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $dbname = "Store";
+
+      try {
+          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          // set the PDO error mode to exception
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $sql = "INSERT INTO Customers (username, password)
+          VALUES ('$name', '$pass')";
+          // use exec() because no results are returned
+          $conn->exec($sql);
+          echo "New record created successfully";
+          }
+      catch(PDOException $e)
+          {
+          echo $sql . "<br>" . $e->getMessage();
+          }
+
+      $conn = null;    
+          }
+      }
 ?>
 
 </body>
