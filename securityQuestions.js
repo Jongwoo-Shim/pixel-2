38033a4d610da857fb2a19984ff76b3aab12e255
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2017-11-23 23:12:04 -0500

    Fun Questions

---
+++ securityQuestions.js
@@ -0,0 +1,14 @@
+function updatePage(){
+	var choice = document.getElementByName("questions")
+	var selectedValue = choice.options[choice.selectedIndex].value;
+	if(selectedValue == "q1"):
+	else if(selectedValue == "q2"){
+
+	}
+	else if(selectedValue == "q3"){
+
+	}
+	else{
+
+	}
+}
