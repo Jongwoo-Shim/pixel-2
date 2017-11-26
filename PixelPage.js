f74704eadb31534c2bde312abfefdfa99950e1e2
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2017-11-26 16:00:16 -0500

    Got the js working

---
+++ PixelPage.js
@@ -0,0 +1,21 @@
+var currentDot;
+
+window.addEventListener("load", startup, false);
+function startup(){
+  selectedColour = document.querySelector("#selectedColour");
+  selectedColour.addEventListener("change", sendUpdate, false);
+  selectedColour.select();
+}
+
+$(".dot").on("click", function() {
+  currentDot = $(this);
+  $('#selectedColour')[0].click();
+});
+
+$('#selectedColour').on('input', function(event) {
+    currentDot.css("background-color", event.delegateTarget.value);
+});
+
+function sendUpdate(event) {
+  alert("hello World")
+}
--- generate.py
+++ generate.py
@@ -10,14 +10,14 @@ def gridGenerator(n):
 		f.write("<body>\n")
 		f.write("\t<div class = \"grid\">\n")
 		f.write("\t<ul class = row>\n")
-		for i in range(n):
+		for i in range(n +1):
 			if i != 0:
 				f.write("\t<ul> \n")
 			for i in range(n + 1):
 				f.write("\t<li class = \"block\"> </li>\n")
 			f.write("\t</ul>\n")
-		f.write("\t</div>")
-		f.write("\t<input type=\"color\" name=\"selectedColour\" value=\"#0000\">\n\t<input type=\"submit\">\n")
+		f.write("\t</div>\n")
+		f.write("\t<input type=\"color\" name=\"selectedColour\" value=\"#0000\">\n")
 		f.write("</body>\n")
 		f.write("</html>")
 gridGenerator(input())
