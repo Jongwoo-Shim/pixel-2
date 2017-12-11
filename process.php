41c8a5c854d3326f15cb238cabe4740da12f6908
OSSYULYYZ <yimeng.zhu@mail.mcgill.ca>
2017-12-10 20:31:04 -0500

    TODO: Test with JSON

--- process.php
+++ process.php
@@ -1,4 +1,11 @@
 <?php
+
+header("Content-Type: application/json");
+// build a PHP variable from JSON sent using GET method
+$arr = json_decode(stripslashes($_GET["colours.json"]));
+// encode the PHP variable to JSON and send it back on client-side
+// echo json_encode($v);
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -6,23 +13,34 @@ $dbname = "Store";
 
 
 $con = mysqli_connect($servername, $username, $password, $dbname) or die("Error you suck " . mysqli_error($con));
-$result1 = mysqli_query($con, "SELECT username FROM Customers WHERE username = '".$name."'");
 
-if (!$result1)
+// for (var prop in obj) {
+//     alert(prop + " is " + obj[prop]);
+// }
+
+for (var $slot in $arr)
 {
-  die(mysqli_error($con));
+    $colour = $arr[$slot];
+    $result1 = mysqli_query($con, "SELECT slot FROM Customers WHERE slot = '".$slot."'");
+
+    if (!$result1)
+    {
+      die(mysqli_error($con));
+    }
+
+    if(mysqli_num_rows($result1) > 0 )
+    {
+      mysqli_query($con,"UPDATE Colours SET colour='$colour' WHERE slot='$slot'");
+    }
+    else
+    {
+    	mysqli_query($con,"INSERT INTO Colours (slot, colour)
+    	VALUES ('$slot', '$colour')");
+    }
 }
 
-if(mysqli_num_rows($result1) > 0 )
-{
-  mysqli_query($con,"UPDATE Colours SET colour='$colour' WHERE slot='$slot'");
-}
-else
-{
-	mysqli_query($con,"INSERT INTO Colours (slot, colour)
-	VALUES ('$slot', '$colour')");
-}
+mysqli_query($con,"SELECT slot FROM Customers FOR JSON AUTO");
 
 $con = null;
-break;
+// break;
 ?>
